57bfd43317bea4c16dd7a74da30674e188a41582
paulovictor1997 <paulovictornunes97@gmail.com>
2022-08-23 20:10:45 -0300

    Update config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     define('HOST', 'sql10.freemysqlhosting.net');
-    define('USER', 'sql10513252');
-    define('PASS', '8fkFZMlCE5');
-    define('BASE', 'sql10513252');
+    define('USER', 'sql10514268');
+    define('PASS', 'JqPdxwFMFf');
+    define('BASE', 'sql10514268');
 
     $conn = new mysqli(HOST,USER,PASS,BASE);
     
